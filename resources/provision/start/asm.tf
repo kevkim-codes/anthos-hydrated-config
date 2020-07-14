9e07c199117c7d36589594f730f81488d8b90a4c
Christopher Grant <crgrant@gmail.com>
2020-07-14 14:36:13 -0500

    initial commit

--- resources/provision/start/asm.tf
+++ resources/provision/start/asm.tf
@@ -1,8 +1,8 @@
 module "asm" {
-  source           = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/asm?ref=add-asm-module"
+  source           = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/asm"
   project_id       = data.google_client_config.current.project
   cluster_name     = module.stage.name
   location         = module.stage.location
   cluster_endpoint = module.stage.endpoint
-  use_tf_google_credentials_env_var = true
+  use_tf_google_credentials_env_var = false
 }
--- resources/provision/start/cluster.tf
+++ resources/provision/start/cluster.tf
@@ -1,16 +1,30 @@
- 
+ data "google_project" "project" {
+  project_id = var.project_id
+}
+
 # Cluster
 module "prod-primary" {
-  name               = "${var.gke_name}-prod-primary"
-    project_id        = module.project-services.project_id
-  source  = "terraform-google-modules/kubernetes-engine/google"
-  regional            = false
+  name                    = "${var.gke_name}-prod-primary"
+  project_id              = module.project-services.project_id
+  source                  = "terraform-google-modules/kubernetes-engine/google"
+  regional                = false
   region                  = var.default_region
   network                 = var.network
   subnetwork              = var.subnetwork
   ip_range_pods           = var.ip_range_pods
   ip_range_services       = var.ip_range_services
-  zones             = var.default_zone
+  zones                   = var.default_zone
+  #release_channel         = "RAPID"
+  node_pools = [
+    {
+      name         = "default-pool"
+      autoscaling  = false
+      auto_upgrade = true
+      # ASM requires minimum 4 nodes and e2-standard-4
+      node_count   = 4
+      machine_type = "e2-standard-4"
+    },
+  ]
 
 }
 # Cluster Credentials
@@ -23,8 +37,8 @@ resource "null_resource" "configure_kubectl_prod_primary" {
 
 # Provision Stage Cluster
 module "stage" {
-  source                  = "terraform-google-modules/kubernetes-engine/google"
-  project_id        = module.project-services.project_id
+  source                  = "terraform-google-modules/kubernetes-engine/google//modules/beta-public-cluster"
+  project_id              = module.project-services.project_id
   name                    = "${var.gke_name}-stage"
   regional                = false
   region                  = var.default_region
@@ -33,7 +47,18 @@ module "stage" {
   ip_range_pods           = var.ip_range_pods
   ip_range_services       = var.ip_range_services
   zones             = var.default_zone
-
+  cluster_resource_labels = { "mesh_id" : "proj-${data.google_project.project.number}" }
+  #release_channel         = "RAPID"
+  node_pools = [
+    {
+      name         = "default-pool"
+      autoscaling  = false
+      auto_upgrade = true
+      # ASM requires minimum 4 nodes and e2-standard-4
+      node_count   = 4
+      machine_type = "e2-standard-4"
+    },
+  ]
 }
 
 # Retrieve Cluster Credentials

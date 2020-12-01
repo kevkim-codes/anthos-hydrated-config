2c03f73eef10df835fd4fd5eec6f9efe1b3de7f1
Kevin Kim <kevkim@google.com>
2020-12-01 16:58:29 -0500

    change terraform prep script

--- labs/platform/clusters/prep.sh
+++ labs/platform/clusters/prep.sh
@@ -23,8 +23,8 @@ cp -R $BASE_DIR/resources/repos/hydrated-config $WORK_DIR
 cd $WORK_DIR/hydrated-config
 git init && git add . && git commit -m "initial commit"
 $BASE_DIR/labs/common/gh.sh create $REPO_PREFIX-hydrated-config  
-git remote add origin $GIT_BASE_URL/$REPO_PREFIX-hydrated-config
-git push origin master
+# git remote add origin $GIT_BASE_URL/$REPO_PREFIX-hydrated-config
+git push origin main
 cd $BASE_DIR
 
 # Create Terraform directory
